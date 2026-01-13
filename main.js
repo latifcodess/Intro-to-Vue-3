51aa99a3a3c40661cac285a6cf98fb3ce612cab4
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-01-13 14:32:32 +0100

    chapitre 1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'fih shirt',
+            description: 'stylish fih shirt'
+        }
+    }
+})
+
 const product = 'Socks'
